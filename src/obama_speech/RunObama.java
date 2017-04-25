bbfeff52d2c313fb796abcd72c2bf0d900b8aed7
DarkNormal <mark.lordan@gmail.com>
2017-04-25 20:46:39 +0100

    Obama speech stored in a hash map

--- src/obama_speech/RunObama.java
+++ src/obama_speech/RunObama.java
@@ -2,7 +2,11 @@ package obama_speech;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  * Created by mark on 24/04/17.
@@ -11,15 +15,26 @@ public class RunObama {
 
 
     public static void main(String[] args) {
+        HashMap<String, Integer> wordCount = new HashMap<>();
         String filename = "/home/mark/IdeaProjects/Java-Bootcamp/src/obama_speech/obama.txt";
         try(Scanner scanner = new Scanner(new File(filename))){
-            while(scanner.hasNext()){
-                System.out.println(scanner.nextLine());
+            while(scanner.hasNextLine()){
+                String[] scannedLine = scanner.nextLine().split(" ");
+                for(String word : scannedLine){
+                    word = word.replaceAll("[^\\w\\s-]", "").toLowerCase();
+                    int frequency = 0;
+                    if(wordCount.containsKey(word)){
+                        frequency = (wordCount.get(word)) +1;
+                    }
+                    wordCount.put(word, frequency + 1);
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
+        for(Map.Entry<String, Integer> entry : wordCount.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
 
     }
 }
