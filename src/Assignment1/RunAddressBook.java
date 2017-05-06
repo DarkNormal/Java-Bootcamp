07c313fec69e4175d12aa83d1ca0eaa973c6b3e3
DarkNormal <mark.lordan@me.com>
2017-05-06 12:11:57 +0100

    Input loop for deciding what operation to execute

--- src/Assignment1/RunAddressBook.java
+++ src/Assignment1/RunAddressBook.java
@@ -4,6 +4,7 @@ import Assignment1.data.FileOperations;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.UUID;
 
 /**
@@ -11,7 +12,12 @@ import java.util.UUID;
  */
 public class RunAddressBook {
 
+
+
     public static void main(String[] args) {
+
+        boolean doMoreOperations = true;
+        int userChoice;
 
         Address commonAddress = new Address("address 1", "address 2", "address 3", "zipcode");
         Contact contact = new Contact(UUID.randomUUID(),"Mark","0123456789", "mark@email.com", commonAddress);
@@ -20,10 +26,35 @@ public class RunAddressBook {
 
         ArrayList<Contact> contacts = fileOperations.readFromFile();
         System.out.println(contacts.size() + " contacts read");
+        Scanner input = new Scanner(System.in);
+
+
+        while(doMoreOperations){
+            System.out.println("What would you like to do? \n1: Add a new contact \n2: Edit an existing contact \n3: Remove a contact \n4: Look up contacts \n5: Exit");
+            try {
+                userChoice = Integer.valueOf(input.nextLine());
+                if(userChoice < 1 || userChoice > 5)
+                    throw new NumberFormatException();
+            } catch (NumberFormatException nfe){
+                System.out.println("Invalid input, please enter the corresponding number for your desired action");
+                continue;
+            }
+            calculateChoice(userChoice);
 
 
 
-        System.out.println();
+            doMoreOperations = false;
+        }
     }
+
+    static void calculateChoice(int choice){
+        switch(choice){
+            case 1:
+
+        }
+
+    }
+
+
 
 }
